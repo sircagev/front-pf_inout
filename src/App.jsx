d005c670d994295a3720e39750bb60ccf27d0220
sircagev <cf.giron04@gmail.com>
2024-03-07 09:34:19 -0500

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Página de inicio</p>
+    </>
+  )
+}
+
+export default App
