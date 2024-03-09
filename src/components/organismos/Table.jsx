4cef45b32915b8c438ae70d7ad774271dd041353
cervantesjuand <juancervantes.1389@gmail.com>
2024-03-09 17:46:19 -0500

    tabla update

--- src/components/organismos/Table.jsx
+++ src/components/organismos/Table.jsx
@@ -36,6 +36,7 @@ export const Table = () => {
                         </button>
                     </td>
                 </tr>
+                
                 </tbody>
             </table>
     </div>
