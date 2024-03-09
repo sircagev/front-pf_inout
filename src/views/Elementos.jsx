bc6ceea3fb5b823eaaf17a310117b9dab98e5f81
cervantesjuand <juancervantes.1389@gmail.com>
2024-03-09 18:32:42 -0500

    añadir modals

--- src/views/Elementos.jsx
+++ src/views/Elementos.jsx
@@ -1,22 +1,57 @@
 import React from 'react'
 import { Table } from '../components/organismos/Table'
 import { TableHidder } from '../components/organismos/TableHidder'
-import Table2 from '../components/organismos/Table2'
+import { Button } from '../components/moleculas/Button'
+import Modal from 'react-modal';
 
 export const Elementos = () => {
 
-  const columnsInfo = ['Codigo','Nombre','Stock','Tipo','Vencimiento','Categoria','Detalle Ubicacion']
-  const dataInfo = [ {'Codigo': 23,'Nombre':23,'Stock':445,'Tipo':565,'Vencimiento':657,'Categoria':65757,'Detalle Ubicacion':474},
-  {'Codigo': 23,'Nombre':23,'Stock':445,'Tipo':565,'Vencimiento':657,'Categoria':65757,'Detalle Ubicacion':474},
-  {'Codigo': 23,'Nombre':23,'Stock':445,'Tipo':565,'Vencimiento':657,'Categoria':65757,'Detalle Ubicacion':474} ]
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className='w-full flex flex-col items-center mt-10'>
         <div className='w-[90%]'>
+            <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+            overlayClassName="w-[50%]"
+            >   
+                <h2>Hello</h2>
+                <button onClick={closeModal}>close</button>
+                <div>I am a modal</div>
+                <form>
+                <input />
+                <button>tab navigation</button>
+                <button>stays</button>
+                <button>inside</button>
+                <button>the modal</button>
+            </form>
+            </Modal>
+            <div className='flex gap-4 mb-5'>
+                <Button
+                    label="Añadir nuevo Elemento"
+                    bg = "bg-green-500"
+                    hoover= "bg-green-600"
+                    funcion={openModal}
+                />
+                <Button
+                    label="Añadir nuevo Stock"
+                    bg = "bg-[#B6A400]"
+                    hoover= "bg-red-600"
+                />
+            </div>
             <TableHidder/>
             <Table/> 
-            <Table2 columns={columnsInfo} data={dataInfo}></Table2>       
         </div>
-        
     </div>
   )
 }
