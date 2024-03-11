51445665fab02e61aabaf0cff54639155831fa40
sircagev <cf.giron04@gmail.com>
2024-03-11 11:06:12 -0500

    123

--- src/components/moleculas/TableBody.jsx
+++ src/components/moleculas/TableBody.jsx
@@ -5,7 +5,7 @@ const TableBody = ({ data, columns }) => (
     {data.map((row, rowIndex) => (
       <tr key={rowIndex} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
         {columns.map((column, columnIndex) => (
-          <td key={columnIndex} className={`py-4 px-6 ${columnIndex > 5 ? "hidden": ""}`}>
+          <td key={columnIndex} className={`py-4 px-6 ${columnIndex > 2 ? "hidden": ""} ${columnIndex > 3 ? "": ""}`}>
             {row[column]}
           </td>
         ))}
